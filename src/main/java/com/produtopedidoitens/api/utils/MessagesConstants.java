2f09c19d1af47762200e96f6685684e83d92ecf9
vanderleik <vanderleik@yahoo.com.br>
2024-07-09 18:35:40 -0300

    feat: PPI-2 - criando OrderEntity

--- src/main/java/com/produtopedidoitens/api/utils/MessagesConstants.java
+++ src/main/java/com/produtopedidoitens/api/utils/MessagesConstants.java
@@ -5,6 +5,9 @@ public class MessagesConstants {
     public static final String PRODUCT_NAME_NOT_BLANK = "O nome do produto não pode ser vazio";
     public static final String PRODUCT_PRICE_NOT_NULL = "O preço do produto não pode ser nulo";
     public static final String PRODUCT_ACTIVE_NOT_NULL = "O status do produto não pode ser nulo";
+    public static final String ORDER_DATE_NOT_NULL = "A data do pedido não pode ser nula";
+    public static final String ORDER_DATE_STATUS_NOT_NULL = "O status do pedido não pode ser nulo";
+    public static final String ORDER_DISCOUNT_POSITIVE = "O desconto do pedido deve ser um valor positivo";
 
     private MessagesConstants() {
     }
