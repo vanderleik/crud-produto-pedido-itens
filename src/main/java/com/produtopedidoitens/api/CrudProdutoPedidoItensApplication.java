923127a601d852afa4597e4c4b2b1091b2147728
vanderleik <vanderleik@yahoo.com.br>
2024-07-09 15:40:56 -0300

    feat: initial commit

---
+++ src/main/java/com/produtopedidoitens/api/CrudProdutoPedidoItensApplication.java
@@ -0,0 +1,13 @@
+package com.produtopedidoitens.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProdutoPedidoItensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProdutoPedidoItensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/produtopedidoitens/api/CrudProdutoPedidoItensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtopedidoitens.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProdutoPedidoItensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
