de0f47c4b2af93455d05293412e3c5b20564a94f
vanderleik <vanderleik@yahoo.com.br>
2024-07-09 19:15:42 -0300

    feat: PPI-5 - criando migration para a tabela de pedidos

---
+++ src/main/resources/db/migration/V202407091914__create_order_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tborder (
+     idorder UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+     orderdate DATE NOT NULL,
+     status VARCHAR(255) NOT NULL,
+     grossstotal NUMERIC(19,2),
+     discount NUMERIC(19,2) CHECK (discount >= 0),
+     nettotal NUMERIC(19,2),
+     dthreg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     dthalt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     version BIGINT
+);
