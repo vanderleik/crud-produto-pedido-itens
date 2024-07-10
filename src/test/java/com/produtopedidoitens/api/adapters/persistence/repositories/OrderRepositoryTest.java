472fee0a5c817c88d1520b71e3919c5171df2eff
vanderleik <vanderleik@yahoo.com.br>
2024-07-10 16:17:24 -0300

    feat: PPI-12 - criando OrderRepository

---
+++ src/test/java/com/produtopedidoitens/api/adapters/persistence/repositories/OrderRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.produtopedidoitens.api.adapters.persistence.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class OrderRepositoryTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+}
