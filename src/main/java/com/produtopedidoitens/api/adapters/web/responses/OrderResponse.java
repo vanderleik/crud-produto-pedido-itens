e9d1c00f3dd8739f3a5c9c88300755a593315fc5
vanderleik <vanderleik@yahoo.com.br>
2024-07-10 17:38:53 -0300

    feat: PPI-13 - criando Request e Response para o pedido

---
+++ src/main/java/com/produtopedidoitens/api/adapters/web/responses/OrderResponse.java
@@ -0,0 +1,24 @@
+package com.produtopedidoitens.api.adapters.web.responses;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+public record OrderResponse(
+        UUID id,
+        LocalDate orderDate,
+        String status,
+        List<String> items,
+        BigDecimal grossTotal,
+        BigDecimal discount,
+        BigDecimal netTotal,
+        LocalDateTime dthreg,
+        LocalDateTime dthalt,
+        Long version
+) {
+}
