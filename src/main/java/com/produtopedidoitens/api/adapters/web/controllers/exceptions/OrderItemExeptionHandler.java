5fed6cac495ef31a78160277c05a6f3e513f4070
vanderleik <vanderleik@yahoo.com.br>
2024-07-11 15:18:26 -0300

    feat: PPI-18 - adicionando DataIntegrityViolationException no ExceptionHandler

--- src/main/java/com/produtopedidoitens/api/adapters/web/controllers/exceptions/OrderItemExeptionHandler.java
+++ src/main/java/com/produtopedidoitens/api/adapters/web/controllers/exceptions/OrderItemExeptionHandler.java
@@ -5,6 +5,7 @@ import com.produtopedidoitens.api.application.exceptions.BadRequestException;
 import com.produtopedidoitens.api.application.exceptions.OrderItemNotFoundException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -32,5 +33,10 @@ public class OrderItemExeptionHandler {
                 .map(ConstraintViolation::getMessage)
                 .collect(Collectors.joining(", "));
         return ResponseEntity.badRequest().body(new ErrorResponseDTO(messages));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(e.getMessage()));
     }
 }
