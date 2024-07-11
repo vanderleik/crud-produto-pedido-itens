83de1a1974e039d501108ef1482ecfb7b6e80664
vanderleik <vanderleik@yahoo.com.br>
2024-07-11 15:18:57 -0300

    feat: PPI-18 - refatorando OrderItemRequest

--- src/main/java/com/produtopedidoitens/api/adapters/web/requests/OrderItemRequest.java
+++ src/main/java/com/produtopedidoitens/api/adapters/web/requests/OrderItemRequest.java
@@ -2,12 +2,10 @@ package com.produtopedidoitens.api.adapters.web.requests;
 
 import lombok.Builder;
 
-import java.math.BigDecimal;
-
 @Builder
 public record OrderItemRequest(
         Integer quantity,
         String productId,
-        BigDecimal price
+        String orderId
 ) {
 }
