5f3ebe6aabe4ba596f0590521525db120e15695d
vanderleik <vanderleik@yahoo.com.br>
2024-07-11 12:35:18 -0300

    feat: PPI-18 - criando OrderItemExeptionHandler

---
+++ src/main/java/com/produtopedidoitens/api/adapters/web/controllers/exceptions/OrderItemExeptionHandler.java
@@ -0,0 +1,36 @@
+package com.produtopedidoitens.api.adapters.web.controllers.exceptions;
+
+import com.produtopedidoitens.api.adapters.web.responses.ErrorResponseDTO;
+import com.produtopedidoitens.api.application.exceptions.BadRequestException;
+import com.produtopedidoitens.api.application.exceptions.OrderItemNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class OrderItemExeptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponseDTO> handleBadRequestException(BadRequestException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(OrderItemNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleOrderItemNotFoundException(OrderItemNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleConstraintViolationException(ConstraintViolationException e) {
+        String messages = e.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(messages));
+    }
+}
