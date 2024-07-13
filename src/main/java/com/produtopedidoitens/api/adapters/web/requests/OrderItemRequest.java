7b842cdb90099618848af46fccf35827968b1b9a
vanderleik <vanderleik@yahoo.com.br>
2024-07-13 09:30:42 -0300

    feat: PPI-29 - adicionando documentação às requests

--- src/main/java/com/produtopedidoitens/api/adapters/web/requests/OrderItemRequest.java
+++ src/main/java/com/produtopedidoitens/api/adapters/web/requests/OrderItemRequest.java
@@ -1,11 +1,19 @@
 package com.produtopedidoitens.api.adapters.web.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record OrderItemRequest(
+
+        @Schema(description = "Quantidade do produto em unidades inteiras", example = "10", required = true)
         Integer quantity,
+
+        @Schema(description = "Id do produto obtido ao cadastrar um produto", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6", required = true)
         String productId,
+
+        @Schema(description = "Id do pedido obtido ao cadastrar um pedido", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6", required = true)
         String orderId
+
 ) {
 }
