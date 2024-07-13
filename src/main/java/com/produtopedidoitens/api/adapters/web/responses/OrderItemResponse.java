f69828d0dddf123520df74fbb247188abb7f3cd1
vanderleik <vanderleik@yahoo.com.br>
2024-07-13 09:31:13 -0300

    feat: PPI-29 - adicionando documentação às responses

--- src/main/java/com/produtopedidoitens/api/adapters/web/responses/OrderItemResponse.java
+++ src/main/java/com/produtopedidoitens/api/adapters/web/responses/OrderItemResponse.java
@@ -1,5 +1,6 @@
 package com.produtopedidoitens.api.adapters.web.responses;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -7,11 +8,33 @@ import java.util.UUID;
 
 @Builder
 public record OrderItemResponse(
+
+        @Schema(description = "Id do item do pedido gerado automaticamente ao se cadastrar um item no pedido", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID id,
+
+        @Schema(description = "Produto do item do pedido", example = "\"product\": {\n" +
+                "\t\t\"id\": \"a4ecbe67-a74c-49b7-9662-862798ba34d4\",\n" +
+                "\t\t\"productName\": \"Segurança\",\n" +
+                "\t\t\"price\": 100.00,\n" +
+                "\t\t\"type\": \"Serviço\",\n" +
+                "\t\t\"active\": true,\n" +
+                "\t\t\"dthreg\": \"2024-07-11T19:45:27.386352\",\n" +
+                "\t\t\"dthalt\": \"2024-07-11T19:45:27.386352\",\n" +
+                "\t\t\"version\": 0\n" +
+                "\t},")
         ProductResponse product,
+
+        @Schema(description = "Quantidade do item do pedido (em unidades)", example = "1")
         Integer quantity,
+
+        @Schema(description = "Data e hora de registro do item do pedido", example = "2024-07-11T19:45:27.386352")
         LocalDateTime dthreg,
+
+        @Schema(description = "Data e hora da última alteração do item do pedido", example = "2024-07-11T19:45:27.386352")
         LocalDateTime dthalt,
+
+        @Schema(description = "Versão do item do pedido", example = "0")
         Long version
+
 ) {
 }
--- src/main/java/com/produtopedidoitens/api/adapters/web/responses/OrderResponse.java
+++ src/main/java/com/produtopedidoitens/api/adapters/web/responses/OrderResponse.java
@@ -1,24 +1,48 @@
 package com.produtopedidoitens.api.adapters.web.responses;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Builder
 public record OrderResponse(
+        @Schema(description = "Id do Pedido gerado automaticamente ao se cadastrar um pedido", example = "123e4567-e89b-12d3-a456-426614174000")
         UUID id,
+
+        @Schema(description = "Número do Pedido gerado automaticamente ao se cadastrar um pedido", example = "PED-988-2024")
         String orderNumber,
+
+        @Schema(description = "Data do Pedido", example = "2024-12-31")
         LocalDate orderDate,
+
+        @Schema(description = "Status do Pedido", example = "Aberto")
         String status,
-//        List<OrderItemResponse> items,
+
+        @Schema(description = "Itens do Pedido")
+        List<OrderItemResponse> items,
+
+        @Schema(description = "Valor Total Bruto do Pedido", example = "100.00")
         BigDecimal grossTotal,
+
+        @Schema(description = "Percentual de Desconto aplicado sobre os produtos do pedido", example = "10.00")
         BigDecimal discount,
-//        BigDecimal netTotal,
+
+        @Schema(description = "Valor Total Líquido do Pedido", example = "90.00")
+        BigDecimal netTotal,
+
+        @Schema(description = "Data de Registro do Pedido", example = "2024-12-31T23:59:59")
         LocalDateTime dthreg,
+
+        @Schema(description = "Data de Alteração do Pedido", example = "2024-12-31T23:59:59")
         LocalDateTime dthalt,
+
+        @Schema(description = "Versão do Pedido", example = "1")
         Long version
+
 ) {
 }
